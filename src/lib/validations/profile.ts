f5e06cb3b05b2a1230b76c89e48ec28be9a69c20
Twahaaa <abubakkertwaha@gmail.com>
2025-11-19 15:42:39 +0530

    refactor(onboarding): add a select field to college

--- src/lib/validations/profile.ts
+++ src/lib/validations/profile.ts
@@ -1,11 +1,19 @@
 import { z } from "zod";
+import { engineeringColleges } from "../college";
+
 
 export const profileSchema = z.object({
   name: z.string().min(1, "Name is required"),
   section: z.string().optional(),
   branch: z.string().optional(),
   year: z.string().optional(),
-  college: z.string().optional(),
+  // college: z.string().optional(),
+
+  college: z.string().refine(
+    (val) => engineeringColleges.includes(val),
+    { message: "Please select a valid college from the list." }
+  ),
+
   bio: z.string().max(300, "Bio cannot exceed 300 characters").optional(),
   image: z.string().url("Invalid URL for image").optional().or(z.literal("")),
   skills: z.array(z.string()).optional(),
